03b2eb32feaa1fa7e94c8283b18beded457c21b5
Laura Eckman <leckman@mit.edu>
2017-04-24 09:55:57 -0400

    display alternating rs/ws chart

--- public/js/formatPatternForDisplay.js
+++ public/js/formatPatternForDisplay.js
@@ -1,20 +1,38 @@
 // returns html string
-formatPattern = function(pattern) {
+formatRSPattern = function(pattern) {
   var rows = pattern.rows;
-  var htmlTable = "<div id='pattern-table'><table class='pattern table'>";
+  var htmlTable = "<table class='pattern table'>";
   rows.forEach(function(row, i) {
     var htmlRow = "<tr id='"+getIdOfRow(i)+"'>";
     var stitches = row.stitches;
     stitches.forEach(function(stitch, j){
       var topTag = "<td id='"+getIdOfStitch(i,j)+"'>";
-      var contents = stitch.symbol;
+      var contents = stitch.r_symbol;
       var botTag = "</td>";
       htmlRow = htmlRow + topTag + contents + botTag;
     });
     htmlRow += "</tr>";
     htmlTable += htmlRow;
   });
-  return htmlTable+"</table></div>";
+  return htmlTable+"</table>";
+};
+
+formatWSPattern = function(pattern) {
+  var rows = pattern.rows;
+  var htmlTable = "<table class='pattern table'>";
+  rows.forEach(function(row, i) {
+    var htmlRow = "<tr id='"+getIdOfRow(i)+"'>";
+    var stitches = row.stitches;
+    stitches.forEach(function(stitch, j){
+      var topTag = "<td id='"+getIdOfStitch(i,j)+"'>";
+      var contents = stitch.w_symbol;
+      var botTag = "</td>";
+      htmlRow = htmlRow + topTag + contents + botTag;
+    });
+    htmlRow += "</tr>";
+    htmlTable += htmlRow;
+  });
+  return htmlTable+"</table>";
 };
 
 getIdOfStitch = function(row, col) {
