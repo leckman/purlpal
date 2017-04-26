5fb6d590490c2b312f53c1132c6951522d119ace
Laura Eckman <leckman@mit.edu>
2017-04-26 13:01:13 -0400

    tracking works with needles, still a little oversensitive

--- public/js/leap.js
+++ public/js/leap.js
@@ -8,10 +8,12 @@ var t_since_last_stitch = 0;
 var t_since_armed = 0;
 var t_threshold = 2;
 
-var y_threshold = 2; // boundary around middle calibration point
-var v_threshold = 40; // velocity constraint on detection
+var y_threshold = 8; // boundary around middle calibration point
+var v_max_threshold = 40; // velocity constraint on detection
+var v_min_threshold = 15;
 
 var tipPosition = 0;
+var tipVelocity = 40;
 var tipCoordinate = 2; // vertical if leap is pointed at you
 var tipSum = 0;
 var tipAvg = 0;
@@ -28,20 +30,36 @@ var controller = Leap.loop({frame: function(frame) {
       // looking at y coordinate
       var newTipPosition = frame.tools[0].tipPosition[tipCoordinate];
       tipPosition = (newTipPosition + tipPosition)/2; // to further reduce noise, average samples
-      var newTipVelocity = frame.tools[0].tipVelocity[tipCoordinate];
+      var newTipVelocity = Math.abs(frame.tools[0].tipVelocity[tipCoordinate]);
+      tipVelocity = (newTipVelocity + tipVelocity)/2;
 
-      if ((tipPosition + y_threshold > tipAvg) && !inStitchSequence) {
-        console.log("Beginning Stitch Sequence");
-        inStitchSequence = true;
-      } else if ((t % 100) === 0) {
-        console.log("Y: " + tipPosition + " avg: " + tipAvg);
-        //console.log(frame.tools[0].tipPosition);
+      if (t % 1000 === 0) {
+        console.log("     Timestep Update");
+        console.log("         min_v: " + v_min_threshold);
+        console.log("          velo: " + tipVelocity);
+        console.log("         max_v: " + v_max_threshold);
+        console.log("      y_thresh: " + (tipAvg - y_threshold));
+        console.log("             y: " + tipPosition);
+        console.log("      y_thresh: " + tipAvg + y_threshold);
       }
 
-      if ((tipPosition < tipAvg + y_threshold) && inStitchSequence) {
+      if ((tipPosition - y_threshold > tipAvg) && !inStitchSequence) {
+        console.log("######Beginning Stitch Sequence######");
+        console.log("    min_v: " + v_min_threshold);
+        console.log("     velo: " + tipVelocity);
+        console.log("    max_v: " + v_max_threshold);
+        console.log("        y: " + tipPosition);
+        console.log(" y_thresh: " + tipAvg + y_threshold);
+        inStitchSequence = true;
+      }
+
+      if ((tipPosition < tipAvg - y_threshold) && inStitchSequence) {
         console.log("######Stitch Counted######");
-        console.log("      velo: " + newTipVelocity);
-        console.log("      y: " + tipPosition);
+        console.log("     min_v: " + v_min_threshold);
+        console.log("      velo: " + tipVelocity);
+        console.log("     max_v: " + v_max_threshold);
+        console.log("  y_thresh: " + (tipAvg - y_threshold));
+        console.log("         y: " + tipPosition);
         inStitchSequence = false;
       }
     }
