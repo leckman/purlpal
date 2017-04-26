2b3ed6f520c55ef835e84494712bcc3520130e11
Laura Eckman <leckman@mit.edu>
2017-04-26 13:48:33 -0400

    removed velocity constraints, added time constraints to stitch detection

--- public/js/leap.js
+++ public/js/leap.js
@@ -1,19 +1,17 @@
 console.log("Ready to Detect Leap Motion Data");
 
 var calibrationPeriod = 50; // time to calibrate to tool location before detection starts
-var sampleFrequency = 5; // to reduce noise, only sample every 5 frames
+var sampleFrequency = 4; // to reduce noise, only sample every 5 frames
 
-// TODO make sure stitches aren't registered too close in time (eliminate duplicates)
+// make sure stitches aren't registered too close in time (eliminate duplicates)
+// all values in terms of sampled frames
 var t_since_last_stitch = 0;
 var t_since_armed = 0;
-var t_threshold = 2;
+var t_threshold = 5;
 
 var y_threshold = 8; // boundary around middle calibration point
-var v_max_threshold = 40; // velocity constraint on detection
-var v_min_threshold = 15;
 
 var tipPosition = 0;
-var tipVelocity = 40;
 var tipCoordinate = 2; // vertical if leap is pointed at you
 var tipSum = 0;
 var tipAvg = 0;
@@ -30,37 +28,36 @@ var controller = Leap.loop({frame: function(frame) {
       // looking at y coordinate
       var newTipPosition = frame.tools[0].tipPosition[tipCoordinate];
       tipPosition = (newTipPosition + tipPosition)/2; // to further reduce noise, average samples
-      var newTipVelocity = Math.abs(frame.tools[0].tipVelocity[tipCoordinate]);
-      tipVelocity = (newTipVelocity + tipVelocity)/2;
 
       if (t % 1000 === 0) {
         console.log("     Timestep Update");
-        console.log("         min_v: " + v_min_threshold);
-        console.log("          velo: " + tipVelocity);
-        console.log("         max_v: " + v_max_threshold);
         console.log("      y_thresh: " + (tipAvg - y_threshold));
         console.log("             y: " + tipPosition);
         console.log("      y_thresh: " + tipAvg + y_threshold);
       }
 
-      if ((tipPosition - y_threshold > tipAvg) && !inStitchSequence) {
+      if ((tipPosition - y_threshold > tipAvg) && !inStitchSequence && (t_since_last_stitch > t_threshold)) {
         console.log("######Beginning Stitch Sequence######");
-        console.log("    min_v: " + v_min_threshold);
-        console.log("     velo: " + tipVelocity);
-        console.log("    max_v: " + v_max_threshold);
-        console.log("        y: " + tipPosition);
-        console.log(" y_thresh: " + tipAvg + y_threshold);
+        console.log("           y: " + tipPosition);
+        console.log("    y_thresh: " + tipAvg + y_threshold);
+        console.log("Elapsed Time: " + t_since_last_stitch);
+        t_since_armed = 0;
         inStitchSequence = true;
+      } else {
+        t_since_armed += 1;
       }
 
-      if ((tipPosition < tipAvg - y_threshold) && inStitchSequence) {
+      if ((tipPosition < tipAvg - y_threshold) && inStitchSequence && (t_since_armed > t_threshold)) {
         console.log("######Stitch Counted######");
-        console.log("     min_v: " + v_min_threshold);
-        console.log("      velo: " + tipVelocity);
-        console.log("     max_v: " + v_max_threshold);
-        console.log("  y_thresh: " + (tipAvg - y_threshold));
-        console.log("         y: " + tipPosition);
+        console.log("    y_thresh: " + (tipAvg - y_threshold));
+        console.log("           y: " + tipPosition);
+        console.log("Elapsed Time: " + t_since_armed);
+
         inStitchSequence = false;
+        t_since_last_stitch = 0;
+        advanceStitch();
+      } else {
+        t_since_last_stitch += 1;
       }
     }
     t+=1; // only increment if tool is present
