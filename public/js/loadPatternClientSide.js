2c4b2bf25d9cbc555cc83f4d7c814ed3ea054860
Laura Eckman <leckman@mit.edu>
2017-04-26 23:22:03 -0400

    more manual control of table view

--- public/js/loadPatternClientSide.js
+++ public/js/loadPatternClientSide.js
@@ -27,25 +27,82 @@ $(function(){
             $("#" + getIdOfRow(pattern.current_row)).toggleClass("selectedRow");
             $("#" + getIdOfStitch(pattern.current_row, pattern.current_stitch)).toggleClass("selectedStitch");
             patternLoaded = true;
-            $("#pattern-container").append("<button id='advance-stitch-button' type='button' class='btn'>Advance Stitch</button>");
-            $("#pattern-container").append("<button id='advance-row-button' type='button' class='btn'>Advance Row</button>");
-            $( "#advance-stitch-button" ).click(function() {
-              advanceStitch();
+            $("#pattern-container").append("<div id='movement-buttons'></div>");
+            $("#movement-buttons").append("<button id='advance-stitch-button' type='button' class='btn'>Advance Stitch</button>");
+            $("#movement-buttons").append("<button id='advance-row-button' type='button' class='btn'>Advance Row</button>");
+            $( "#advance-stitch-button" ).click(advanceStitch);
+            $( "#advance-row-button" ).click(advanceRow);
+            $(".pattern td").dblclick(function() {
+              selectId($(this).attr("id"));
             });
-            $( "#advance-row-button" ).click(function() {
-              advanceRow();
-            });
+            $("#pattern-container").append("<div id='view-buttons'></div>");
+            $("#view-buttons").append("<button id='rs-only-button' type='button' class='btn'>Display Right Side Only</button>");
+            $("#view-buttons").append("<button id='ws-only-button' type='button' class='btn'>Display Wrong Side Only</button>");
+            $("#view-buttons").append("<button id='view-toggle-button' type='button' class='btn'>Display Active Side of Pattern</button>");
+            $("#rs-only-button").click(rs_only);
+            $("#ws-only-button").click(ws_only);
+            $("#view-toggle-button").click(toggle_view);
           }
         );
       }
     );
   }
+var toggle = false;
+rs_only = function(){
+  $("#pattern-table").empty();
+  $("#pattern-table").append(rs_pattern);
+  $(".pattern td").dblclick(function() {
+    selectId($(this).attr("id"));
+  });
+  $("#" + getIdOfRow(pattern.current_row)).toggleClass("selectedRow");
+  $("#" + getIdOfStitch(pattern.current_row, pattern.current_stitch)).toggleClass("selectedStitch");
+  toggle = false;
+};
+
+ws_only = function(){
+  $("#pattern-table").empty();
+  $("#pattern-table").append(ws_pattern);
+  $(".pattern td").dblclick(function() {
+    selectId($(this).attr("id"));
+  });
+  $("#" + getIdOfRow(pattern.current_row)).toggleClass("selectedRow");
+  $("#" + getIdOfStitch(pattern.current_row, pattern.current_stitch)).toggleClass("selectedStitch");
+  toggle = false;
+};
+
+toggle_view = function(){
+  $("#pattern-table").empty();
+  if (pattern.current_row % 2 === 0) { // rs row
+    $("#pattern-table").append(rs_pattern);
+  } else {
+    $("#pattern-table").append(ws_pattern);
+  }
+  $(".pattern td").dblclick(function() {
+    selectId($(this).attr("id"));
+  });
+  $("#" + getIdOfRow(pattern.current_row)).toggleClass("selectedRow");
+  $("#" + getIdOfStitch(pattern.current_row, pattern.current_stitch)).toggleClass("selectedStitch");
+  toggle = true;
+};
+
+selectId = function(id){
+  // remove old styling
+  $("#" + getIdOfStitch(pattern.current_row, pattern.current_stitch)).toggleClass("selectedStitch");
+  $("#" + getIdOfRow(pattern.current_row)).toggleClass("selectedRow");
+  // add new styling
+  var breakdown = id.split("-");
+  pattern.current_row = Number(breakdown[1]);
+  pattern.current_stitch = Number(breakdown[2]);
+  $("#" + getIdOfStitch(pattern.current_row, pattern.current_stitch)).toggleClass("selectedStitch");
+  $("#" + getIdOfRow(pattern.current_row)).toggleClass("selectedRow");
+};
 
 advanceStitch = function(){
   if (!patternLoaded) {
     console.log("Hold on! Still setting up.");
     return;
   }
+  console.log("attempting advance");
 
   var current_row_length= $("#" + getIdOfRow(pattern.current_row) + " td").length;
   if (pattern.current_stitch + 1 < current_row_length) {
@@ -64,17 +121,22 @@ advanceRow = function(){
     return;
   }
 
-  //$("#" + getIdOfRow(pattern.current_row)).toggleClass("selectedRow");
-  //$("#" + getIdOfStitch(pattern.current_row, pattern.current_stitch)).toggleClass("selectedStitch");
+  $("#" + getIdOfRow(pattern.current_row)).toggleClass("selectedRow");
+  $("#" + getIdOfStitch(pattern.current_row, pattern.current_stitch)).toggleClass("selectedStitch");
 
   pattern.current_row += 1;
   pattern.current_stitch = 0;
 
-  $("#pattern-table").empty();
-  if (pattern.current_row % 2 === 0) { // rs row
-    $("#pattern-table").append(rs_pattern);
-  } else {
-    $("#pattern-table").append(ws_pattern);
+  if(toggle){
+    $("#pattern-table").empty();
+    if (pattern.current_row % 2 === 0) { // rs row
+      $("#pattern-table").append(rs_pattern);
+    } else {
+      $("#pattern-table").append(ws_pattern);
+    }
+    $(".pattern td").dblclick(function() {
+      selectId($(this).attr("id"));
+    });
   }
 
   $("#" + getIdOfRow(pattern.current_row)).toggleClass("selectedRow");
